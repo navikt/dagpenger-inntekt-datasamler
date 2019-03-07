e05a994b658c6f90962fbec73355d4ee99909037
Atle Nordland <atle.nordland@nav.no>
2019-03-07 13:40:16 +0100

    Fix inntektApi rest url

--- src/main/kotlin/no/nav/dagpenger/datalaster/inntekt/Environment.kt
+++ src/main/kotlin/no/nav/dagpenger/datalaster/inntekt/Environment.kt
@@ -4,7 +4,7 @@ data class Environment(
     val username: String = getEnvVar("SRVDP_DATALASTER_INNTEKT_USERNAME"),
     val password: String = getEnvVar("SRVDP_DATALASTER_INNTEKT_PASSWORD"),
     val oicdStsUrl: String = getEnvVar("OIDC_STS_ISSUERURL"),
-    val inntektApiUrl: String = getEnvVar("DAGPENGER_INNTEKT_API_URL"),
+    val inntektApiUrl: String = getEnvVar("DAGPENGER_INNTEKT_API_REST_URL"),
     val bootstrapServersUrl: String = getEnvVar(
         "KAFKA_BOOTSTRAP_SERVERS",
         "localhost:9092"
