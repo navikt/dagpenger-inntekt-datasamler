4d8fa24c9e8b916a3d0caec959edc3e2c5c93d46
Geir André Lund <geir.andre.lund@nav.no>
2019-03-18 19:07:07 +0100

    Update streams

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
 
-    implementation("no.nav.dagpenger:streams:0.3.0-SNAPSHOT")
+    implementation("no.nav.dagpenger:streams:0.3.1-SNAPSHOT")
     implementation("no.nav.dagpenger:events:0.3.1-SNAPSHOT")
 
     compile("org.apache.kafka:kafka-clients:$kafkaVersion")
