5ad156655c7a2578823c4daada2a76b1640a29ed
Atle Nordland <atle.nordland@nav.no>
2019-08-05 13:43:26 +0200

    Put spesifisert inntekt on packet

--- src/main/kotlin/no/nav/dagpenger/datalaster/inntekt/JsonAdapters.kt
+++ src/main/kotlin/no/nav/dagpenger/datalaster/inntekt/JsonAdapters.kt
@@ -2,6 +2,8 @@ package no.nav.dagpenger.datalaster.inntekt
 
 import com.squareup.moshi.JsonAdapter
 import no.nav.dagpenger.events.inntekt.v1.Inntekt
+import no.nav.dagpenger.events.inntekt.v1.SpesifisertInntekt
 import no.nav.dagpenger.events.moshiInstance
 
 val inntektJsonAdapter: JsonAdapter<Inntekt> = moshiInstance.adapter(Inntekt::class.java)
+val spesifisertInntektJsonAdapter: JsonAdapter<SpesifisertInntekt> = moshiInstance.adapter(SpesifisertInntekt::class.java)
