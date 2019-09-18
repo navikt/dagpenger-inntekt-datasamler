1de84eda8f8a2e8e9bf0bdc334ed37358a5ec459
Geir André Lund <geir.andre.lund@nav.no>
2019-09-18 12:02:43 +0200

    Fix broker DNS address: a01apvl150.adeo.no --> a01apvl00150.adeo.no

--- src/main/kotlin/no/nav/dagpenger/datalaster/inntekt/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/datalaster/inntekt/Configuration.kt
@@ -31,7 +31,7 @@ private val prodProperties = ConfigurationMap(
     mapOf(
         "application.profile" to "PROD",
         "dp.inntekt.api.url" to "http://dp-inntekt-api/",
-        "kafka.bootstrap.servers" to "a01apvl00145.adeo.no:8443,a01apvl00146.adeo.no:8443,a01apvl00147.adeo.no:8443,a01apvl00148.adeo.no:8443,a01apvl00149.adeo.no:8443,a01apvl150.adeo.no:8443"
+        "kafka.bootstrap.servers" to "a01apvl00145.adeo.no:8443,a01apvl00146.adeo.no:8443,a01apvl00147.adeo.no:8443,a01apvl00148.adeo.no:8443,a01apvl00149.adeo.no:8443,a01apvl00150.adeo.no:8443"
     )
 )
 
